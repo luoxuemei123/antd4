19971098f3b54c9a473df77bbc680f37ee436a63
洛雪 <luoxm@scmsafe.com>
2020-08-28 12:32:39 +0800

    init app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-07-24 11:35:30
+ * @LastEditTime: 2020-08-28 11:03:18
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /antd4/src/App.js
+ */
 import React from 'react';
 import './App.less';
 import {
@@ -6,6 +14,7 @@ import {
 import Admin from './pages/admin/admin.jsx'
 import Login from './pages/login/login.jsx'
 function App() {
+  
   return (
     <div>
       <BrowserRouter>
